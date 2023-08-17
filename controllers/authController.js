1c839bb35e286f9afc5c6222d1fa046d16c82854
Humberto Castellon <mcastellon@unan.edu.ni>
2023-08-17 14:13:00 -0600

    add auth and err msg in login page

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../config/database');
+const bcrypt = require('bcrypt');
 
 
 // Route to display the login form
@@ -11,21 +12,29 @@ router.get('/login', (req, res) => {
 // Route to handle the login form submission
 router.post('/login', async (req, res) => {
     const { username, password } = req.body;
+    
 
     try {
-        const rows = await db.query('SELECT * FROM sys.usuarios WHERE username = ? AND password = ?', [username, password]);
-
+        const [rows] = await db.query('SELECT * FROM sys.usuarios WHERE username = ?', [username]);
+        //console.log(rows)
         if (rows.length > 0) {
-            // user authenticated, save session info
-            req.session.user = rows[0];
-            res.redirect('/dashboard'); // 
+            const user = rows[0];
+            //console.log(user.password);
+            const match = await bcrypt.compare(password, user.password);
+            if (match) {
+                req.session.user = user;
+                res.redirect('/dashboard');
+            } else {
+                res.render('login', { error: 'Acceso inválido. Por favor, inténtelo otra vez.' });
+            }
         } else {
-            res.redirect('/login');
+            res.render('login', { error: 'Acceso inválido. Por favor, inténtelo otra vez.' });
         }
     } catch (error) {
         console.error(error);
         res.status(500).send('Internal Server Error');
     }
-    });
+
+});
 
 module.exports = router;
